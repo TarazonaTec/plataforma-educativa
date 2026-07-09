5a63018c5babb35ad8a0a1a8b87689b785fb91c3
TarazonaTec <69410832+TarazonaTec@users.noreply.github.com>
2026-07-09 10:01:00 -0500

    Update media.js

--- js/components/media.js
+++ js/components/media.js
@@ -1,1 +1,51 @@
+function mostrarMedia(
+    contenedor,
+    datos
+){
 
+    contenedor.innerHTML = "";
+
+    if(datos.imagen){
+
+        contenedor.innerHTML = `
+            <img
+                src="${datos.imagen}"
+                class="pregunta-img"
+            >
+        `;
+
+        return;
+
+    }
+
+    if(datos.video){
+
+        contenedor.innerHTML = `
+            <video
+                class="pregunta-video"
+                controls
+            >
+                <source
+                    src="${datos.video}"
+                    type="video/mp4">
+            </video>
+        `;
+
+        return;
+
+    }
+
+    if(datos.youtube){
+
+        contenedor.innerHTML = `
+            <iframe
+                width="315"
+                height="576"
+                src="https://www.youtube.com/embed/${datos.youtube}"
+                allowfullscreen>
+            </iframe>
+        `;
+
+    }
+
+}
