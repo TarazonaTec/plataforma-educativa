4e2396cf66dea2bc0b029931eaf5d37c3beebe29
TarazonaTec <69410832+TarazonaTec@users.noreply.github.com>
2026-07-09 09:49:26 -0500

    Update options.js

--- js/components/options.js
+++ js/components/options.js
@@ -1,1 +1,24 @@
+function mostrarOpciones(contenedor, opciones, callback) {
 
+    // Limpiar opciones anteriores
+    contenedor.innerHTML = "";
+
+    opciones.forEach((opcion, index) => {
+
+        const boton = document.createElement("button");
+
+        boton.className = "opcion";
+
+        boton.textContent = opcion;
+
+        boton.addEventListener("click", () => {
+
+            callback(index);
+
+        });
+
+        contenedor.appendChild(boton);
+
+    });
+
+}
